beb77cdd3822041d8e90b68865f33d41e85c117c
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-11 13:44:49 -0400

    add a sign in form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -12,13 +12,39 @@ class SignIn extends React.Component {
         }
     }
 
+    handleSubmit = event => {
+        event.preventDefault()
+
+        this.setState({email:'', password: ''})
+    }
+
+    handleChange = event => {
+        const {value, name} = event.target
+        this.setState({ [name]:value })
+    }
+
     render(){
         return (
         <div className='sign-in'>
             <h2> I already have an account</h2>
             <span>Sign in with your email and password</span>
+            
+            <form onSubmit={this.handleSubmit}>
+                <input name='email' value ={this.state.email} required />
+                <label> Email</label>
+                <input 
+                name='password' 
+                type='password' 
+                value ={this.state.email} 
+                onChange = {this.handleChange}
+                required />
+                <label>email</label>
+
+                <input type='submit' value='Submit Form'/>
+            </form>
         </div>
         )
     }
 }
 
+export default SignIn
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 import './sign-in-and-sign-up.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component'
 
 const SignInAndSignUpPage = () => (
-    <div className=''> sign/page</div>
+    <div className='sign-in-and-sign-up'> 
+        <SignIn/>
+    </div>
 )
 
 export default SignInAndSignUpPage
