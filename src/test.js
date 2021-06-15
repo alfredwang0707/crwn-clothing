bc72e7a0908f0a8d109d7ae909727747f4e93a75
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-14 22:13:09 -0400

    testing firebase calls / docs

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firestore = firebase.firestore()
+
+firestore.collection('users').doc('Fmh6fmBgkrJbGMgYrtaw')
+// firestore.doc('/users//users/Fmh6fmBgkrJbGMgYrtaw')
+ 
