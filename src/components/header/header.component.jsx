152061a7bc395ddba8e8ca8caca7ea51d92ffb21
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-14 21:29:26 -0400

    add css for sign in buttons

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,8 @@ import './header.styles.scss'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo}  from '../../assets/crown.svg'
 
-const Header = () => (
+import { auth } from '../../firebase/firebase.utils'
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo' />
@@ -15,6 +16,12 @@ const Header = () => (
         <Link className = 'option' to='/shop'>
             CONTACT
         </Link>
+        {
+            currentUser ? 
+            <div className='option' onClick={()=> auth.signOut()}> SIGN OUT </div>
+            :
+            <Link className='option' to='/signin'> SIGN IN </Link>
+        }
         </div>
     </div>
 )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -49,12 +49,14 @@ class SignIn extends React.Component {
                  required 
                  label='password'
                  />
-
-                <CustomButton type='submit' > Sign In </CustomButton>
-                <CustomButton onClick={signInWithGoogle} > 
-                {' '}
-                Sign in with Google {' '}
-                 </CustomButton>
+                <div className='buttons'>
+                    <CustomButton type='submit' > Sign In </CustomButton>
+                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn > 
+                    {' '}
+                    Sign in with Google {' '}
+                    </CustomButton>   
+                </div>
+           
 
             </form>
         </div>
