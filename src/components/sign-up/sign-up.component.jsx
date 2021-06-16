b205f0bec01cb2b2af115f9140cf6fbd32bbe8c7
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-15 21:07:36 -0400

    fixed bug from sign up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -17,43 +17,42 @@ class SignUp extends React.Component {
             confirmPassword: ''
         }
     }
+    
+    handlesubmit = async event => {
+        event.preventDefault()
+    
+    const {displayName, email, password, confirmPassword} = this.state
+
+    if(password !== confirmPassword){
+        alert("passwords don't match")
+        return
+    }
+
+    try {
+        const { user } = await auth.createUserWithEmailAndPassword(email,password)
+        
+        await createUserProfileDocument(user, {displayName})
+
+        this.setState({
+            displayName: '', 
+            email: '',
+            password: '',
+            confirmPassword: ''
+        })
+
+    } catch(error){
+        console.error(error)
+    }
+
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target
+        this.setState({[name]: value})
+    }
 
     render (){
-
-        const {displayName, email, password, confirmPassword}
-
-        handlesubmit = async event => {
-            event.preventDefault()
-        
         const {displayName, email, password, confirmPassword} = this.state
-
-        if(password !== confirmPassword){
-            alert("passwords don't match")
-            return
-        }
-
-        try {
-            const { user } = await auth.createUserWithEmailAndPassword(email,password)
-            
-            await createUserProfileDocument(user, {displayName})
-
-            this.setState({
-                displayName: '', 
-                email: '',
-                password: '',
-                confirmPassword: ''
-            })
-
-        } catch(error){
-            console.error(error)
-        }
-
-        }
-
-        handleChange = event => {
-            const {name, value} = event.target
-            this.setState({[name]: value})
-        }
 
         return (
             <div className='sign-up'>
