13d03d89dfa9698162bdcc9e25506f2a3b84ebdf
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-16 22:27:14 -0400

    add empty shopping cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { ReactComponent as Logo}  from '../../assets/crown.svg'
 import { connect } from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon.component'
+import  CartDropdown  from '../cart-dropdown/cart-dropdown.component'
 
 import { auth } from '../../firebase/firebase.utils'
 const Header = ({ currentUser }) => (
@@ -26,6 +27,7 @@ const Header = ({ currentUser }) => (
         }
         <CartIcon/>
         </div>
+        <CartDropdown/ >
     </div>
 )
 
