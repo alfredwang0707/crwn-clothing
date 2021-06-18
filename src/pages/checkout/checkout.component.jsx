c759a1b2a33d206cc1adabcf0c59142d4350d5be
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-18 13:30:17 -0400

    add styles for checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {createStructuredSelector } from 'reselect'
+
+import { selectCartItems, selectCartItemsCount } from '../../redux/cart/cart.selectors' 
 
 import './checkout.styles.scss'
 
 const CheckoutPage = () => (
-    <div>
-        CHECKOUT PAGE
+    <div className='checkout-page'>
+        <div className='checkout-header'>
+            <div className='header-block'>
+                <span> Product </span>
+            </div>
+            <div className='header-block'>
+                <span> Description </span>
+            </div>
+            <div className='header-block'>
+                <span> Quantity </span>
+            </div>
+            <div className='header-block'>
+                <span> Price </span>
+            </div>
+            <div className='header-block'>
+                <span> Remove </span>
+            </div>
+
+        </div>
     </div>
 )
+
+const mapStateToProps = createStructuredSelector({
+    cartItems: selectCartItems
+})
 
  export default CheckoutPage
