4de7e6885f458b286491b3f9e77c6deeb71c809b
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-17 22:30:28 -0400

    fixed typo in cart util

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,9 +4,9 @@ export const addItemToCart =(cartItems, cartItemToAdd) => {
     if(existingCartItem){
         return cartItems.map(cartItem=> 
             cartItem.id === cartItemToAdd.id
-            ? { ...cartItem, quntity: cartItem.quntity +1}
+            ? { ...cartItem, quantity: cartItem.quantity +1}
             : cartItem)
     }
 
-    return [...cartItems, {...cartItemToAdd, quntity:1 }]
-}
+    return [...cartItems, {...cartItemToAdd, quantity:1 }]
+} 
