b8db64c6ea1f973a2c1aa0fdd19f734b23e1e793
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-17 23:09:13 -0400

    add selector/memoization for cart items

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+//memoization
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
