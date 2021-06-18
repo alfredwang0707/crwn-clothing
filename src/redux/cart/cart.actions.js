4849ed86026024d59cbfd99df8629634921ddeb7
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-17 22:16:22 -0400

    cart item updated, cart actions typo fixed

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,7 +1,7 @@
 import CartActionTypes from './cart.types'
 
 export const toggleCartHidden = () => ({
-    type: CartActionTypes.toggleCartHidden
+    type: CartActionTypes.TOGGLE_CART_HIDDEN
 })
 
 export const addItem = item => ({
