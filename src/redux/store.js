42469e638cfb92d565db79ac8fe5ecab1558c583
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-21 22:49:24 -0400

    add persist/config/ maintain, now cart items in lcoal storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
@@ -6,6 +7,7 @@ import rootReducer from './root-reducer'
 
 const middlewares = [logger]
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
-export default store
+export const persistor = persistStore(store)
+
