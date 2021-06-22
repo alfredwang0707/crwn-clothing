7b57407dfd7a9599b0c27765200805da7a0b7ec8
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-21 21:14:06 -0400

    add checkout item page/css, cart with selectors

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,8 @@ import {createStructuredSelector } from 'reselect'
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors' 
 
 import './checkout.styles.scss'
+import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -25,10 +27,9 @@ const CheckoutPage = ({ cartItems, total }) => (
                 <span> Remove </span>
             </div>
         </div>
-        {
-            cartItems.map(cartItem =>
-            cartItem.name)
-        }
+        {cartItems.map(cartItem =>(
+            <CheckoutItem key={cartItem.id} cartItem = {cartItem} />
+        ))}
         <div className='total'>
             <span> TOTAL: ${total}</span>
         </div>
