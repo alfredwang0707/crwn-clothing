988961862375e9eaee7e6ed9a29ca541113e0b2b
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-22 23:28:12 -0400

    add memoize for collection url param

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,7 @@
 import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize'
+
+
 const selectShop = state => state.shop
 
 const COLLECTION_ID_MAP = {
@@ -14,13 +17,15 @@ export const selectCollections = createSelector(
     [selectShop],
     shop => shop.collections
 )
+// by wrapping this function in memoize, if this function gets called with same param
+// dont return since its same value
 
-export const selectCollection = collectionUrlParam => 
-createSelector(
-    [selectCollections],
-    collections => 
-        collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+export const selectCollection = memoize((collectionUrlParam) => 
+    createSelector(
+        [selectCollections],
+        collections => 
+            collections.find(
+                collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+    )
 )
-
 //find collection.id matching the url param of our colleciton id map
