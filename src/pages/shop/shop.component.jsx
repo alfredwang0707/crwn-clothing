3af7410d7682a83145ba5ad1e0387967fb684520
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-22 23:07:42 -0400

    fixed naming convention error

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-const ShopPage = ({ collections}) =>(
+import CollectionPage from '../collection/collection.component'
+
+const ShopPage = ({ match }) =>(
     <div className='shop-page'>
-        <CollectionsOverview />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )  
     
