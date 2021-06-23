ee75f6bfe53d93eb82ea9359b1ac6fdabd4471da
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-22 21:48:42 -0400

    moved directory into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,16 +4,18 @@ import storage from 'redux-persist/lib/storage'
 
 import userReducer from './user/user.reducer'
 import cartReducer from './cart/cart.reducer'
+import directoryReducer from './directory/directory.reducer'
 
 const persistConfig= {
-    key: 'root',
+    key: 'root', 
     storage, 
     whitelist: ['cart']
 }
 
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    diretory: directoryReducer
 })
 
 
