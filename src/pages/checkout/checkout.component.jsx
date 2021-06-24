b22ebb68a04d7e378215679c136cdda9d32e5e68
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-23 23:15:26 -0400

    update stripe, favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -37,7 +37,7 @@ const CheckoutPage = ({ cartItems, total }) => (
             * Please use the following test credit card for payments *
         </div>
         <br /> 
-        4242 4242 4242 4242 - Exp 01/20 CVV: 123
+        4242 4242 4242 4242 - Exp 01/22 CVV: 123
         <StripeCheckoutButton  price={total} />
     </div>
 )
