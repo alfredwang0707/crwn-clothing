31e9d8389f037aca002f6a812938bac4e084a52a
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-24 16:06:53 -0400

    update homepage to styled component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
+import { HomePageContainer } from './homepage.styles'
+
 const HomePage =( ) => (
-    <div className = 'homepage'>
-        <Directory />
-    </div>
-    
+    <HomePageContainer >
+        <Directory />  
+    </HomePageContainer>
 )
 
 export default HomePage
