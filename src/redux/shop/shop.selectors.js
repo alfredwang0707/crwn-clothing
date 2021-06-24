376d161ae20eba4fc8ddd2ba8c2cb77e93a75a9e
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-23 22:11:36 -0400

    changing shop data into json

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -15,25 +15,19 @@ export const selectCollectionsForPreview = createSelector(
   collections => Object.keys(collections).map(key => collections[key])
 )
 
-const COLLECTION_ID_MAP = {
-  hats:1,
-  sneakers:2,
-  jackets:3,
-  womens:4,
-  mens:5 
-}
 
-export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    collections => collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
-  ) 
+
+// export const selectCollection = memoize((collectionUrlParam) =>
+//   createSelector(
+//     [selectCollections],
+//     collections => collections.find(
+//       collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+//     )
+//   ) 
+// )
+export const selectCollection =collectionUrlParam =>
+createSelector(
+  [selectCollections],
+  collections => collections[collectionUrlParam]
 )
-// export const selectCollection = memoize((collectionUrlParam)
-// createSelector(
-//   [selectCollections],
-//   (collections) => collections[collectionUrlParam]
-// ))
 
