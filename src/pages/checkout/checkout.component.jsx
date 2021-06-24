17cbcb0ff23e3ab405e4150ab6a661cdc9c64a9c
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-23 23:07:52 -0400

    add style for stripe testing card on checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -33,6 +33,11 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem = {cartItem} />
         ))}
         <div className='total'>TOTAL: ${total} </div>
+        <div className='test-warning'>
+            * Please use the following test credit card for payments *
+        </div>
+        <br /> 
+        4242 4242 4242 4242 - Exp 01/20 CVV: 123
         <StripeCheckoutButton  price={total} />
     </div>
 )
