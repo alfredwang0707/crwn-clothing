5aa69468f7618b5e2440ecb2e90a658be67c0fc7
alfredwang0707 <alfredwang0707@gmail.com>
2021-06-29 19:41:16 -0400

    create with spinner and css

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -12,7 +12,7 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
+  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 )
 
 
@@ -25,9 +25,9 @@ export const selectCollectionsForPreview = createSelector(
 //     )
 //   ) 
 // )
-export const selectCollection =collectionUrlParam =>
+export const selectCollection = collectionUrlParam =>
 createSelector(
   [selectCollections],
-  collections => collections[collectionUrlParam]
+  collections => (collections ? collections[collectionUrlParam] : null)
 )
 
