76ad0528af7b7df5413b2754380691e85db76ffb
alfredwang0707 <alfredwang0707@gmail.com>
2021-07-01 01:08:30 -0400

    move code out of async from thunk

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,13 +1,41 @@
-import {takeEvery} from 'redux-saga/effects'
+import {takeEvery, call, put} from 'redux-saga/effects'
 
 import ShopActionTypes from './shop.types'
+import { firestore, convertCollectionSnapshotToMap} from '../../firebase/firebase.utils'
+
+import {
+    fetchCollectionsSuccess,
+    fetchCollectionsFailure
+
+} from './shop.actions'
 
 export function* fetchCollectionAsync(){
-    yield console.log('i am fired');
+    yield console.log('i am fired')
+    
+    try {     
+        const collectionRef = firestore.collection('collections') 
+        const snapshot = yield collectionRef.get()
+        const collectionsMap = yield call (convertCollectionSnapshotToMap, snapshot)
+        yield put(fetchCollectionsSuccess(collectionsMap))
+        
+    } catch(error){
+        yield put(fetchCollectionsFailure(error.message))
+    } 
+     
+
+   
+//            collectionRef
+//             .get()
+//             .then(snapshot => {
+//                const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+//                dispatch(fetchCollectionsSuccess(collectionsMap))
+//              });   
+        
+//    
 }
 
 export function* fetchCollectionsStart(){
     yield takeEvery(
         ShopActionTypes.FETCH_COLLECTIONS_START, 
         fetchCollectionAsync)
-}
+} 
