1a024dfefc80d4e5263634b9ff655cd9b9eca286
alfredwang0707 <alfredwang0707@gmail.com>
2021-07-01 22:31:43 -0400

    modify user reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,28 +1,34 @@
-import  UserActionTypes  from './user.types'
+import UserActionTypes from './user.types'
 
-const INITIAL_STATE ={
-    currentUser: null,
-    error: null
+const INITIAL_STATE = {
+  currentUser: null,
+  error: null
 }
-const userReducer = (state = INITIAL_STATE, action ) => {
-    switch(action.type){
-        case UserActionTypes.GOOGLE_SIGN_IN_SUCCESS:
-        case UserActionTypes.EMAIL_SIGN_IN_SUCCESS:
-            return {
-                ...state, 
-                currentUser: action.payload,
-                error: null
-            }
-        case UserActionTypes.GOOGLE_SIGN_IN_FAILURE:
-        case UserActionTypes.EMAIL_SIGN_IN_FAILURE:
 
-            return {
-                ...state,
-                error: action.payload
-            }
-        default:
-            return state
-    }
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UserActionTypes.SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        currentUser: action.payload,
+        error: null
+      }
+    case UserActionTypes.SIGN_OUT_SUCCESS:
+      return {
+        ...state,
+        currentUser: null,
+        error: null
+      }
+    case UserActionTypes.SIGN_IN_FAILURE:
+    case UserActionTypes.SIGN_OUT_FAILURE:
+    case UserActionTypes.SIGN_UP_FAILURE:
+      return {
+        ...state,
+        error: action.payload
+      }
+    default:
+      return state
+  }
 }
 
 export default userReducer
